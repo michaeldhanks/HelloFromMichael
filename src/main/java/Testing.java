b9b6f56c9e71bba22e1f5abd6a3985acdc434425
dstrick97 <60444868+dstrick97@users.noreply.github.com>
2020-01-31 13:13:17 -0700

    Update Testing.java

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,5 +1,6 @@
 public class Testing {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Your name");
+        System.out.println("Your name is my name?... We are one");
     }
 }
